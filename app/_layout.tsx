5f9e033e0476f56be35a860cfa6aec179f029e2a
G <geraldineaustinsemail@gmail.com>
2025-06-26 10:53:24 +1200

    Fix useFrameworkReady import path

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useAuth } from '../hooks/useAuth';
 import { LoadingSpinner } from '../components/ui/LoadingSpinner'
-import { useFrameworkReady } from '@/hooks/useFrameworkReady';
+import { useFrameworkReady } from '../hooks/useFrameworkReady';
 
 export default function RootLayout() {
   useFrameworkReady();
