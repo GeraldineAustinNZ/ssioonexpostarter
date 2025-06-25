43244c97e7b1382d4e7ed1c10151242a6a1fe567
G <geraldineaustinsemail@gmail.com>
2025-06-25 22:18:13 +1200

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,9 +2,11 @@ import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { useAuth } from '../hooks/useAuth';
-import { LoadingSpinner } from '../components/ui/LoadingSpinner';
+import { LoadingSpinner } from '../components/ui/LoadingSpinner'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
+  useFrameworkReady();
   const { loading } = useAuth();
 
   if (loading) {
